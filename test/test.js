f490de6df7b23c009eb3249d45710191b8bf300c
nicolas <nicolas.massai@etu.univ-amu.fr>
2024-09-06 10:41:21 +0200

    init10

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const request = require('supertest')
-const app = require('../../src/hello')
+const app = require('../src/hello')
 
 describe('GET /hello/:name', () => {
   test('It should respond with a welcome message with the name', async () => {
